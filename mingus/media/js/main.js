9777a9bf2efd82bf5dd520958ec59d50a21ad994
Florent Messa <florent.messa@gmail.com>
2010-12-30 15:14:49 +0100

    CSS transition for main menu instead of javascript

--- mingus/media/js/main.js
+++ mingus/media/js/main.js
@@ -1,30 +1,3 @@
-(function($){
-    $.extend($.fx.step, {
-        backgroundPosition: function(fx) {
-            if(fx.state === 0 && typeof fx.end == 'string'){
-                var start = $.curCSS(fx.elem, 'backgroundPosition');
-                start = toArray(start);
-                fx.start = [start[0], start[2]];
-                var end = toArray(fx.end);
-                fx.end = [end[0], end[2]];
-                fx.unit = [end[1], end[3]];
-            }
-            var nowPosX = [];
-            nowPosX[0] = ((fx.end[0] - fx.start[0]) * fx.pos) + fx.start[0] + fx.unit[0];
-            nowPosX[1] = ((fx.end[1] - fx.start[1]) * fx.pos) + fx.start[1] + fx.unit[1];
-            fx.elem.style.backgroundPosition = nowPosX[0] + ' ' + nowPosX[1];
-
-           function toArray(strg){
-               strg = strg.replace(/left|top/g, '0px');
-               strg = strg.replace(/right|bottom/g, '100%');
-               strg = strg.replace(/([0-9\.]+)(\s|\)|$)/g, '$1px$2');
-               var res = strg.match(/(-?[0-9\.]+)(px|\%|em|pt)\s(-?[0-9\.]+)(px|\%|em|pt)/);
-               return [parseFloat(res[1], 10), res[2], parseFloat(res[3],10), res[4]];
-           }
-        }
-    });
-})(jQuery)
-
 $(document).ready(function(){
     prettyPrint();
 
@@ -58,22 +31,6 @@ $(document).ready(function(){
                 .end()
                 .toggleClass('share-hide')
         });
-
-    $('ul#nav-menu li.nav-item  a').each(function(){
-        var backgroundPositionX = $(this).css('backgroundPosition').split(' ')[0];
-        $(this)
-            .mouseover(function(){
-                $(this)
-                    .stop()
-                    .animate({backgroundPosition: '('+ backgroundPositionX +' -432px)'}, {duration: 300});
-            })
-            .mouseout(function(){
-                $(this)
-                    .stop()
-                    .animate({backgroundPosition: '('+ backgroundPositionX +' -416px)'}, {duration: 300});
-            });
-    });
-
 
     $('ul li.title h2')
         .click(function(){
